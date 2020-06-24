436c336a2c7279b5c767e7bcf2eeb9283c922e8b
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2020-06-24 14:35:23 -0300

    Sending tags, environment, custom data and params using `Conduit.Message` attributes to appsignal

--- lib/plug/subscriber.ex
+++ lib/plug/subscriber.ex
@@ -20,7 +20,38 @@ defmodule ConduitAppsignal.Plug.Subscriber do
 
   def call(message, next, opts) do
     action = "#{inspect(opts[:subscriber])}.process"
-    Appsignal.Transaction.set_action(action)
+
+    tags = %{
+      source: message.source,
+      destination: message.destination,
+      user_id: message.user_id,
+      correlation_id: message.correlation_id,
+      message_id: message.message_id
+    }
+
+    environment =
+      Map.merge(
+        %{
+          content_type: message.content_type,
+          content_encoding: message.content_encoding,
+          created_by: message.created_by,
+          created_at: message.created_at
+        },
+        message.headers
+      )
+
+    custom = %{
+      assigns: message.assigns,
+      private: message.private
+    }
+    
+    action
+    |> Appsignal.Transaction.set_action()
+    |> Appsignal.Transaction.set_sample_data("environment", environment)
+    |> Appsignal.Transaction.set_sample_data("tags", tags)
+    |> Appsignal.Transaction.set_sample_data("params", message.body)
+    |> Appsignal.Transaction.set_sample_data("custom_data", custom)
+
     instrument("message.process", action, fn ->
       next.(message)
     end)
